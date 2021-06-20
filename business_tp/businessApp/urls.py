acfa4d118c3d0512c870338c1c3f791a8caca302
Mateo Aquino <57442760+mateoaq@users.noreply.github.com>
2021-06-20 18:37:43 -0300

    Siguiendo las anteriores correciones esto deberia quedar así

--- business_tp/businessApp/urls.py
+++ business_tp/businessApp/urls.py
@@ -3,6 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('businessApp/', include('businessApp.urls')),
-    path('business/', views.business, name='business'),
 ]
